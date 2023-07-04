11f9554dbefb96e22d6dab3c5b4089b94d911577
CaraRota <canalsdigital@gmail.com>
2023-07-04 13:22:33 -0300

    first commit - v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Calculator from './components/calculator/Calculator'
+
+function App() {
+
+  return (
+    <>
+      <Calculator />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import "../../css/Calculator.css"
+
+import { Box, Button, InputLabel, InputAdornment, OutlinedInput, FormControl, Alert } from '@mui/material'
+import TelegramIcon from '@mui/icons-material/Telegram';
+
+const Calculator = () => {
+    const [envios, setEnvios] = useState("")
+    const [costoEnvio, setCostoEnvio] = useState(200)
+    const [efectivo, setEfectivo] = useState("")
+    const [totalEfectivo, setTotalEfectivo] = useState("")
+    const [alert, setAlert] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (envios.length > 0 && efectivo.length > 0 && totalEfectivo.length > 0) {
+
+            const totalCostoEnvio = envios * costoEnvio
+
+            const headerMsg = "*NiCKi's cierre de caja:*" + "\n\n";
+            const msgEnvios = `Envios: ${envios}` + "\n"
+            const msgCostoEnvio = `Total: $${totalCostoEnvio.toLocaleString("es-AR")}` + "\n\n"
+            const msgEfectivo = `Efectivo: $${(efectivo - totalCostoEnvio).toLocaleString("es-AR")}` + "\n\n"
+            const msgTotalEfectivo = `Caja: $${(totalEfectivo - totalCostoEnvio).toLocaleString("es-AR")}`
+
+            const encodedText = encodeURIComponent(headerMsg + msgEnvios + msgCostoEnvio + msgEfectivo + msgTotalEfectivo);
+            const url = `https://wa.me/?text=${encodedText}`;
+            window.open(url);
+            setAlert(false)
+            setEnvios("")
+            setCostoEnvio("")
+            setEfectivo("")
+            setTotalEfectivo("")
+        } else {
+            setAlert(true)
+        }
+    }
+
+    return (
+        <div className='calculator-container'>
+            <div className='calculator-title'>Calculadora de efectivo</div>
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                className='calculator-box'
+                onSubmit={handleSubmit}
+            >
+                <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Nro de Envios</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-amount"
+                        label="Nro de Envios"
+                        color="warning"
+                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                        value={envios}
+                        onChange={(e) => setEnvios(e.target.value)}
+                    />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Costo de Envio</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-amount"
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                        label="Costo de Envio"
+                        color="warning"
+                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                        value={costoEnvio}
+                        onChange={(e) => setCostoEnvio(e.target.value)}
+                    />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Efectivo del Cadete</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-amount"
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                        label="Efectivo del Cadete"
+                        color="warning"
+                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                        value={efectivo}
+                        onChange={(e) => setEfectivo(e.target.value)}
+                    />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Total Efectivo</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-amount"
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                        label="Total Efectivo"
+                        color="warning"
+                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                        value={totalEfectivo}
+                        onChange={(e) => setTotalEfectivo(e.target.value)}
+                    />
+                </FormControl>
+                <Button
+                    className='calculator-btn'
+                    variant="contained"
+                    type='submit'>
+                    Enviar <TelegramIcon />
+                </Button>
+            </Box>
+            {alert && <Alert sx={{ margin: 3 }} variant="outlined" severity="error">Debes rellenar todos los campos</Alert>}
+        </div>
+    )
+}
+
+export default Calculator
