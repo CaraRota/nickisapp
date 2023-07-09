b5b8ea34c86d46c3d9cf4db61f13ab409f94a868
CaraRota <canalsdigital@gmail.com>
2023-07-08 22:33:02 -0300

    Minor changes

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -28,7 +28,7 @@ const Calculator = () => {
             window.open(url);
             setAlert(false)
             setEnvios("")
-            setCostoEnvio("")
+            setCostoEnvio(200)
             setEfectivo("")
             setTotalEfectivo("")
         } else {
@@ -47,47 +47,47 @@ const Calculator = () => {
                 onSubmit={handleSubmit}
             >
                 <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
-                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Nro de Envios</InputLabel>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="success">Nro de Envios</InputLabel>
                     <OutlinedInput
                         id="outlined-adornment-amount"
                         label="Nro de Envios"
-                        color="warning"
+                        color="success"
                         inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                         value={envios}
                         onChange={(e) => setEnvios(e.target.value)}
                     />
                 </FormControl>
                 <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
-                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Costo de Envio</InputLabel>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="success">Costo de Envio</InputLabel>
                     <OutlinedInput
                         id="outlined-adornment-amount"
                         startAdornment={<InputAdornment position="start">$</InputAdornment>}
                         label="Costo de Envio"
-                        color="warning"
+                        color="success"
                         inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                         value={costoEnvio}
                         onChange={(e) => setCostoEnvio(e.target.value)}
                     />
                 </FormControl>
                 <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
-                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Efectivo del Cadete</InputLabel>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="success">Efectivo del Cadete</InputLabel>
                     <OutlinedInput
                         id="outlined-adornment-amount"
                         startAdornment={<InputAdornment position="start">$</InputAdornment>}
                         label="Efectivo del Cadete"
-                        color="warning"
+                        color="success"
                         inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                         value={efectivo}
                         onChange={(e) => setEfectivo(e.target.value)}
                     />
                 </FormControl>
                 <FormControl fullWidth sx={{ m: 1, width: '35ch' }}>
-                    <InputLabel htmlFor="outlined-adornment-amount" color="warning">Total Efectivo</InputLabel>
+                    <InputLabel htmlFor="outlined-adornment-amount" color="success">Total Efectivo</InputLabel>
                     <OutlinedInput
                         id="outlined-adornment-amount"
                         startAdornment={<InputAdornment position="start">$</InputAdornment>}
                         label="Total Efectivo"
-                        color="warning"
+                        color="success"
                         inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                         value={totalEfectivo}
                         onChange={(e) => setTotalEfectivo(e.target.value)}
